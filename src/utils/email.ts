2659668f2c3c0b0e29370cc8d370ecd72ef5c0de
cleantomcd <cleanto.santos.macedo.neto@ccc.ufcg.edu.br>
2026-05-17 10:22:30 -0300

    chore: padroniza erros em errors.ts

--- src/utils/email.ts
+++ src/utils/email.ts
@@ -1,4 +1,5 @@
 import nodemailer from 'nodemailer';
+import { AppError } from './errors';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -9,24 +10,22 @@ const transporter = nodemailer.createTransport({
 });
 
 export async function sendVerificationEmail(toEmail: string, verificationCode: string): Promise<void> {
-  try {
-    await transporter.sendMail({
-      from: '"Sistema K3D" <kria3dk3d@gmail.com>',
-      to: toEmail,
-      subject: 'Seu código de acesso para K3D',
-      // editar o html
-      html: `
-        <div style="font-family: sans-serif; padding: 20px;">
-          <h2>Confirmação de Cadastro</h2>
-          <p>Para ativar sua conta, utilize o código abaixo:</p>
-          <div style="font-size: 24px; font-weight: bold; background-color: #f4f4f4; padding: 10px; display: inline-block;">
-            ${verificationCode}
-          </div>
-          <p>Este código expira em 15 minutos.</p>
+  await transporter.sendMail({
+    from: '"Sistema K3D" <kria3dk3d@gmail.com>',
+    to: toEmail,
+    subject: 'Seu código de acesso para K3D',
+    // editar o html
+    html: `
+      <div style="font-family: sans-serif; padding: 20px;">
+        <h2>Confirmação de Cadastro</h2>
+        <p>Para ativar sua conta, utilize o código abaixo:</p>
+        <div style="font-size: 24px; font-weight: bold; background-color: #f4f4f4; padding: 10px; display: inline-block;">
+          ${verificationCode}
         </div>
-      `,
-    });
-  } catch (error) {
-    throw new Error('Falha no envio do email');
-  }
+        <p>Este código expira em 15 minutos.</p>
+      </div>
+    `,
+  }).catch(() => {
+    throw new AppError('Falha no envio do email', 502);
+  });
 }
