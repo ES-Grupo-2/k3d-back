e016540f886021a4f27c0b1924729b5763b8decc
claraugusta <mc.augusta1052@gmail.com>
2026-05-26 14:11:03 -0300

    feat(kanban): added orderRoutes in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import fastify, { FastifyServerOptions } from "fastify";
 import { authRoutes } from "./modules/auth/auth.routes";
 import { errorHandler } from "./utils/errors";
+import { orderRoutes } from "./modules/order/order.routes";
 
 export function buildApp(options: FastifyServerOptions = { logger: true }) {
   const app = fastify(options);
 
   app.setErrorHandler(errorHandler);
   app.register(authRoutes);
-
+  app.register(orderRoutes);
   return app;
 }
