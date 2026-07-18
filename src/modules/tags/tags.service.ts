12d1f772e9ccc01d4d74601dd8a28f27cff3eded
TaTaCo1 <tarcisioao@gmail.com>
2026-07-18 14:17:15 -0300

    fix: adjust imports prisma in tags

--- src/modules/tags/tags.service.ts
+++ src/modules/tags/tags.service.ts
@@ -1,4 +1,4 @@
-import { Prisma } from "@prisma/client;
+import { Prisma } from "@prisma/client";
 import { prisma } from "../../lib/clientPrisma";
 import { AppError } from "../../utils/errors";
 import { CreateTagInput, TagResponse, UpdateTagInput } from "./tags.types";
